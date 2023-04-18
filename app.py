d606094961d1806464a4d9cd37655543e6fbbd24
Raj Bhatodra <91852356+raj-bhatodra@users.noreply.github.com>
2023-04-18 11:26:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, World! I'm Docker, a platform for developing, and running applications in a containerized environment."
+    return "Hello, World! I'm Docker, a platform for developing, running applications in a containerized environment."
